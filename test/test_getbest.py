84071894c6ff649d5a2f0b38adcce8c1cf0298c3
Oudiematic3000 <oudmayera@gmail.com>
2025-04-07 15:14:27 +0200

    All tests added and functional.

--- test/test_getbest.py
+++ test/test_getbest.py
@@ -1,5 +1,5 @@
 import unittest
-from getbest.getbest import getCols
+from getbest.getbest import getCols, findTop
 
 class TestClass(unittest.TestCase):
     def test_getNumCol(self):
@@ -13,6 +13,18 @@ class TestClass(unittest.TestCase):
         _num_col, _mark_col= getCols(f)
         self.assertEqual(_mark_col,4)
         f.close()
+
+    def test_findTopID(self):
+        f=f=open('test/testdat0.csv', encoding='utf-8')
+        _num_col, _mark_col= getCols(f)
+        _id, _mark = findTop(f, _num_col, _mark_col)
+        self.assertEqual(_id,'167381')
+    
+    def test_findTopMark(self):
+        f=f=open('test/testdat0.csv', encoding='utf-8')
+        _num_col, _mark_col= getCols(f)
+        _id, _mark = findTop(f, _num_col, _mark_col)
+        self.assertEqual(_mark,90)
 
 
 if __name__ == '__main__':
